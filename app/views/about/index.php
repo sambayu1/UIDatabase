08e62e793529f8a0a3fcdf293a473d66ef1d5834
sambayu1 <yogabs8975@gmail.com>
2020-11-27 15:01:05 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+    
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/diluc.jpg" alt="Yoga Bayu S" width="200" class="rounded-circle shadow">
+    <hr class="my-4">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    <p>DeadLine Mepet adalah jalan ninjaku..</p>
+    <p>Namun ketika Deadline mendekat, seketika ide saya langsung meluap..</p>
+    <p>Terimakasih Bu Salehah karena Deadline di perpanjang 2 kali sehingga saya bisa berpikir 2 kali :v</p>
+
+</div>
---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="jumbotron mt-4">
+      <h1 class="display-4">Selamat Datang di Website Pendataan Mahasiswa!</h1>
+      <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
+      <hr class="my-4">
+      <p>Ini hanya sedikit latihan dari diri saya sendiri :)</p>
+      <hr class="my-4">
+    </div>
+
+
+</div>
